eaf56d0fba4392b229c05d6ec2fc79ee3e804359
Stuart Greenhall <stuart_greenhall@hotmail.com>
2016-09-14 07:48:44 +0100

    stu: adding step 3 refactoring

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-module.exports = require('./lib/fizz-buzz-convert.js');
+"use strict";
+
+const FizzbuzzTransform = require('./lib/fizz-buzz-convert.js');
+const FizzbuzzReport = require('./lib/fizz-buzz-report.js');
+
+const onSpace = " ";
+
+function convert(aString) {
+  let numbers = aString.split(onSpace);
+  let luckified = FizzbuzzTransform.luckyNumbers(numbers)
+  let results = FizzbuzzTransform.multiples(luckified);
+  let report = FizzbuzzReport.create(results);
+  console.log(report);
+  return results.join(" ");
+}
+
+module.exports = {
+  convert
+}
--- specs/fizz-buzz-convert-spec.js
+++ specs/fizz-buzz-convert-spec.js
@@ -5,14 +5,18 @@ var FizzBuzz = require('../lib/fizz-buzz-convert');
 
 describe("Fizz Buzz-ing a string thats delimited by a space",() =>{
   it("should convert multiples of 3 into 'fizz'", () => {
-    FizzBuzz.transformMultiples(["1", "2", "3", "4", "6"]).should.eql(["1", "2", "fizz", "4", "fizz"]);
+    FizzBuzz.multiples(["1", "2", "3", "4", "6"]).should.eql(["1", "2", "fizz", "4", "fizz"]);
   });
 
   it("should convert multiples of 5 into 'buzz' ", () => {
-    FizzBuzz.transformMultiples(["5", "8", "10", "11", "20"]).should.eql(["buzz", "8", "buzz", "11" ,"buzz"]);
+    FizzBuzz.multiples(["5", "8", "10", "11", "20"]).should.eql(["buzz", "8", "buzz", "11" ,"buzz"]);
   })
 
   it("should convert multiples of 15 into 'fizzbuzz' ", () => {
-    FizzBuzz.transformMultiples(["15", "8", "30", "11", "22"]).should.eql(["fizzbuzz", "8", "fizzbuzz", "11" ,"22"]);
+    FizzBuzz.multiples(["15", "8", "30", "11", "22"]).should.eql(["fizzbuzz", "8", "fizzbuzz", "11" ,"22"]);
+  })
+
+  it("should convert an number containsing a 3 into 'lucky' ", () => {
+    FizzBuzz.luckyNumbers(["3", "8", "13", "30", "22"]).should.eql(["lucky", "8", "lucky", "lucky" ,"22"]);
   })
 })
